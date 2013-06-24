4912171a1fd8f60955d080459d449a1601e40d9b
neodveelop <neodevelop@gmail.com>
2013-06-23 22:21:55 -0500

    Adding mockito and using in the test

--- src/test/java/com/makingdevs/CalculadoraTests.java
+++ src/test/java/com/makingdevs/CalculadoraTests.java
@@ -1,22 +1,29 @@
 package com.makingdevs;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnit44Runner;
+import org.mockito.Mock;
 
+@RunWith(MockitoJUnit44Runner.class) // Nos ayudamos de este runner en JUnit para usar las anotaciones de Mockito
 public class CalculadoraTests {
+
+  @Mock CacheDeCalculadora cacheDeCalculadora; // Este es mi colaborador
+
+  Calculadora calculadora = new Calculadora();
 
   @Test
   public void pruebaSumaDeDosNumeros(){
-    Calculadora calculadora = new Calculadora();
     Integer resultado = calculadora.suma(4,5);
     assertTrue(9 == resultado);
   }
   
   @Test
   public void pruebaSumaDeDosNumerosGuardandoEnCache(){
-    Calculadora calculadora = new Calculadora();
-    Integer resultado = calculadora.suma(4,5);
-    assertTrue(9 == resultado);
-    // ¿Como comprobamos la llamada a un servicio de cache? 
+    Integer resultado = calculadora.suma(6,2);
+    assertTrue(8 == resultado);
+    verify(cacheDeCalculadora).persistirSuma(6,2); // ¿En verdad se llamo a este método? 
   }
 }
