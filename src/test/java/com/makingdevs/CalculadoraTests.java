bcdbb6eeee455384d05c4a3194e78f9e35d9ac40
neodveelop <neodevelop@gmail.com>
2013-06-23 21:46:29 -0500

    Adding test

--- src/test/java/com/makingdevs/CalculadoraTests.java
+++ src/test/java/com/makingdevs/CalculadoraTests.java
@@ -11,4 +11,12 @@ public class CalculadoraTests {
     Integer resultado = calculadora.suma(4,5);
     assertTrue(9 == resultado);
   }
+  
+  @Test
+  public void pruebaSumaDeDosNumerosGuardandoEnCache(){
+    Calculadora calculadora = new Calculadora();
+    Integer resultado = calculadora.suma(4,5);
+    assertTrue(9 == resultado);
+    // ¿Como comprobamos la llamada a un servicio de cache? 
+  }
 }
