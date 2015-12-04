1da4a8ce28f5ad0c0013911f39044a249a316d20
Francisco Reyes <francisco@makingdevs.com>
2015-12-04 10:58:46 -0600

    Cambio de pruebas de resta

--- src/main/java/com/makingdevs/CacheDeCalculadora.java
+++ src/main/java/com/makingdevs/CacheDeCalculadora.java
@@ -4,4 +4,13 @@ public interface CacheDeCalculadora {
   void persistirSuma(Integer a, Integer b, Integer r);
   boolean existeElResultadoDeSumar(Integer a, Integer b);
   Integer obtenerElResultadoDeSumar(Integer a, Integer b);
+
+  void persistirResta(Integer a, Integer b, Integer r);
+  boolean existeElResultadoDeResta(Integer a, Integer b);
+  Integer obtenerElResultadoDeResta(Integer a, Integer b);
+
 }
+
+
+
+
